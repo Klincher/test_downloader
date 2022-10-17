9c199905f3f9ecd0ff0ba814c7c66cf5787a823d
Klincher <camelotvm48@gmail.com>
2022-10-17 17:36:07 +0300

    video downloader

--- index.js
+++ index.js
@@ -1,26 +1,48 @@
-const fs = require('fs');
+const fs = require("fs");
+const https = require("https");
+// const download = require("download");
 
-var courses = require('./course.json');
+var courses = require("./course.json");
 
 let currentChapter;
 const countAll = courses.length;
 
-courses.forEach(element => {
-    if (element.type === 'chapter') {
-        currentChapter = element;
-        let dir = './' + (countAll - parseInt(element.order)) + ' - ' + element.title;
-        if (!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
-        console.log('Folders is created');
-        return;
+courses.forEach((element) => {
+  if (element.type === "chapter") {
+    currentChapter = element;
+    let dir =
+      "./" + (countAll - parseInt(element.order)) + " - " + element.title;
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
+    console.log("Folders is created");
+    return;
+  }
 
-    let lectureOrder = ((countAll - parseInt(element.order)) - (countAll - parseInt(currentChapter.order)));
+  let lectureOrder =
+    countAll -
+    parseInt(element.order) -
+    (countAll - parseInt(currentChapter.order));
 
-    let dirTitle = './' + (countAll - parseInt(currentChapter.order)) + ' - ' + currentChapter.title + '/' + lectureOrder  + ' - ' + element.title;
+  let dirTitle =
+    "./" +
+    (countAll - parseInt(currentChapter.order)) +
+    " - " +
+    currentChapter.title +
+    "/" +
+    lectureOrder +
+    " - " +
+    element.title;
 
-    if (!fs.existsSync(dirTitle)){
-        fs.mkdirSync(dirTitle);
-    }
+   https.get(element.url, (response) => {
+    console.log('dirTitle + ".mp4" :>> ', dirTitle + ".mp4");
+    const file = fs.createWriteStream(dirTitle + ".mp4");
+    response.pipe(file);
+
+    // after download completed close filestream
+    file.on("finish", () => {
+      file.close();
+      console.log("Download Completed");
+    });
+  });
 });
